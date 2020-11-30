f4ffafb5f5bebf284d470bb8ce3bc94e9852ec40
kento <37926134+kkent030315@users.noreply.github.com>
2020-11-30 18:39:14 +0900

    update readme

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
+![IMAGE](image.png)
+
 # pretty-json
+
+Json Beautifier using [ace.js](https://github.com/ajaxorg/ace) and [nuxt.js](https://github.com/nuxt/nuxt.js)
--- pretty-json/nuxt.config.js
+++ pretty-json/nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: 'Pretty Json | JSON Beautifizer',
+    title: 'Pretty Json | JSON Beautifier',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
